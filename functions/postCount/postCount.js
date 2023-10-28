8f81e5137eeba444ab0b5370326e1f1ee138a599
Kyle <kyle@hitpixel.com>
2023-10-28 21:31:16 +0400

    switch to CommonJs

--- functions/postCount/postCount.js
+++ functions/postCount/postCount.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
     const location = event.headers.referer || event.headers.origin || '';
