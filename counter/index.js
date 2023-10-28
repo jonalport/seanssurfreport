6107635544b3978f9b3a765cf53b32f36f214391
Kyle <kyle@hitpixel.com>
2023-10-28 15:18:41 +0400

    create web component and serverless

---
+++ counter/index.js
@@ -0,0 +1,67 @@
+// Define the template for the web component
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    .retro-counter {
+      font-family: monospace;
+      font-size: 24px;
+    }
+  </style>
+  <div class="retro-counter">
+    Page Hits: <span id="counter">Loading...</span>
+  </div>
+`;
+
+class PageHitCounter extends HTMLElement {
+  constructor() {
+    super();
+
+    // Create a shadow DOM
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    // Get the serverUrl attribute value
+    this.serverUrl = this.getAttribute('server-url');
+
+    // Fetch the page hit count from the server
+    this.fetchHitCount();
+  }
+
+  validateOrigin(){
+    const location = window.location.href;
+    if (!location) return false;
+    return (location.includes('seanssurfreport.netlify.app') ||
+    location.includes('seanssurfreport.com') ||
+    location.includes('http://127.0.0.1:5500/index.html'))
+  }
+
+  fetchHitCount() {
+    if (!this.serverUrl || !this.validateOrigin()) return;
+    fetch(this.serverUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const counterElement = this.shadowRoot.getElementById('counter');
+        if (counterElement) {
+          counterElement.textContent = `🏄‍♀️ ${data.count} 🏄‍♂️`;
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching page hit count:', error);
+      });
+  }
+
+  // Observe changes to the 'serverUrl' attribute
+  static get observedAttributes() {
+    return ['server-url'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'server-url' && oldValue !== newValue) {
+      this.serverUrl = newValue;
+      this.fetchHitCount();
+    }
+  }
+}
+
+// Define the custom element
+customElements.define('page-hit-counter', PageHitCounter);
---
+++ functions/postCount/postCount.js
@@ -0,0 +1,54 @@
+exports.handler = async function(event, context) {
+    const location = event.headers.referer || event.headers.origin || '';
+    
+    const allowedDomains = [
+        'https://seanssurfreport.netlify.app',
+        'https://seanssurfreport.com',
+        'http://127.0.0.1:5500/index.html'
+    ];
+
+    if (allowedDomains.some(domain => location.includes(domain))) {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': location,
+                'Access-Control-Allow-Credentials': true,
+            },
+        };
+
+        try {
+            const url = process.env.GOOGLE_SCRIPT_URL;
+            // UPDATE COUNT
+            const fetched = await fetch(url, options);
+            const result = await fetched.json();
+
+            return {
+                statusCode: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': location,
+                    'Access-Control-Allow-Credentials': true,
+                },
+                body: JSON.stringify({
+                    count: result.count,
+                }),
+            };
+        } catch (e) {
+            console.error(e);
+            return {
+                statusCode: 500,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': location,
+                    'Access-Control-Allow-Credentials': true,
+                },
+            };
+        }
+    } else {
+        return {
+            statusCode: 403,
+            body: 'Forbidden',
+        };
+    }
+};
