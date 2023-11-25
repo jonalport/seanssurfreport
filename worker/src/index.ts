43760caef59d6d8c941710f4364f25eb5289bdee
Kyle <kyle@hitpixel.com>
2023-11-25 17:31:35 +0400

    cloudflare worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,32 +1,83 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wranger dev` in your terminal to start a development server
  * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	surfreport: KVNamespace;
+	PICTURE_BUCKET: R2Bucket;
+	HEADER_PSK: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const key = url.pathname.slice(1);
+
+		const AUTH_HEADER_KEY = "X-Custom-PSK";
+		const AUTH_HEADER_VALUE = `${env.HEADER_PSK}`;
+		const psk = request.headers.get(AUTH_HEADER_KEY);
+		const allowedKeys = (await env.surfreport?.get('allowedKeys'))?.split(',').map(ky => ky.trim());
+
+		// check if key is allowed
+		if (!allowedKeys || !allowedKeys.includes(key)) return new Response("Not Allowed", { status: 403 });
+		
+		// cors headers	
+		const headers = new Headers();
+		headers.set('Access-Control-Allow-Origin', '*');
+		headers.set('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
+		headers.set('Access-Control-Allow-Credentials', 'true');
+
+		
+		switch (request.method) {
+			case 'OPTIONS':
+				return new Response('OK', { headers });
+			case 'PUT':
+				if (psk !== AUTH_HEADER_VALUE) return new Response("Authentication failed", { status: 403 });
+				try {
+					// Read the image data from the request body
+					const imageData = await request.arrayBuffer();
+					const contentType = 'image/jpeg'; 
+					// Put the object in the R2 bucket
+					await env.PICTURE_BUCKET.put(key, imageData, {
+						httpMetadata: { 'contentType': contentType }
+					});
+					return new Response(`Put ${key} successfully!`);
+				} catch (error) {
+					return new Response(`Error uploading ${key}: ${error}`, { status: 500 });
+				}
+			case 'GET':
+				const isPageCount = key === 'pageCount';
+				// fetching the page count
+				if (isPageCount){
+					const currentCount = (await env.surfreport?.get('pageCount')) || 1;
+					const newCount = Number(currentCount) + 1;
+					await env.surfreport.put('pageCount', `${newCount}`);
+					headers.set('Content-Type', 'application/json');
+					return new Response(JSON.stringify({count: newCount}), { headers });
+				}
+				// fetching an image
+				const object = await env.PICTURE_BUCKET.get(key);
+		
+				if (object === null) {
+					return new Response('No image found', { status: 404 });
+				}
+		
+				object.writeHttpMetadata(headers);
+				headers.set('etag', object.httpEtag);
+		
+				return new Response(object.body, {headers});
+
+		
+			default:
+				return new Response('Method Not Allowed', {
+					status: 405,
+					headers: {
+						Allow: 'PUT, GET',
+					},
+				});
+		}
+		
 	},
 };
+
