9426039207b8dcc54fbde9864ca7c4800407e88d
Sean Jahnig <54946085+foilandwater@users.noreply.github.com>
2024-02-06 15:53:26 +0400

    Header/footer changes
    Made some changes to header and footer text properties

--- components/page-hit-counter.js
+++ components/page-hit-counter.js
@@ -3,7 +3,7 @@ const template = document.createElement("template");
 template.innerHTML = `
   <style>
     .retro-counter {
-      font-family: monospace;
+      <!-- font-family: monospace; -->
       font-size: 20px;
     }
 
