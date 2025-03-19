4bbe8b57d1faac01f9095abae9cdd1b6c0cf787d
Sean Jahnig <54946085+foilandwater@users.noreply.github.com>
2025-03-19 09:02:00 +0400

    v12

--- v4/components/site-main.js
+++ v4/components/site-main.js
@@ -1,12 +1,8 @@
 class SiteMain extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `
-            <section>
-                <h2>Main Content</h2>
-                <p>This is the main content area that will resize based on content.</p>
-            </section>
-        `;
+        // Leave innerHTML empty initially; content will be set dynamically
+        this.innerHTML = '';
     }
 }
 
