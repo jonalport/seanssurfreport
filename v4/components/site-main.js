b4e75fcb9ee9796d7ad122b751a6e38ef5feaf93
Sean Jahnig <54946085+foilandwater@users.noreply.github.com>
2025-06-16 11:35:10 +0400

    v70

--- v4/components/site-main.js
+++
@@ -1,9 +0,0 @@
-class SiteMain extends HTMLElement {
-    constructor() {
-        super();
-        // Leave innerHTML empty initially; content will be set dynamically
-        this.innerHTML = '';
-    }
-}
-
-customElements.define('site-main', SiteMain);
